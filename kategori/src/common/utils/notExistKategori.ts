70b28a32c15287fcc6ffc8a4151f234175f25adc
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-04-10 20:41:39 +0700

    feat: add notExistKategori utility in common/utils

--- kategori/src/common/utils/notExistKategori.ts
+++ kategori/src/common/utils/notExistKategori.ts
@@ -1,10 +1,27 @@
+import { HttpStatus, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../../prisma.service';
 
 // buat fungsi cek
-export const notExistKategori = (
+export const notExistKategori = async (
   prisma: PrismaService['kategori'],
   id: number,
   message: string,
 ) => {
-    
+  const data = await prisma.findUnique({
+    where: {
+      id: id,
+    },
+  });
+
+  if (!data) {
+    throw new NotFoundException({
+      success: false,
+      message: message,
+      metadata: {
+        status: HttpStatus.NOT_FOUND,
+      },
+    });
+  }
+
+  return data;
 };
